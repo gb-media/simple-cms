86dbee9f796666e51589c687b08316aeda80c3b4
toofff <prouyann@gmail.com>
2018-11-19 09:32:00 +0100

    [client-symfony] Feat: add package symfony/profiler-pack & syfmony/phpunit-bridge (#11)

--- client-symfony/config/bundles.php
+++ client-symfony/config/bundles.php
@@ -4,4 +4,6 @@ declare(strict_types=1);
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
 ];
